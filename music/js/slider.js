d87bf95dcbb6e8c30fbf8bcad34add12bf70485d
fkrhtmq123 <fkrhtmq123@naver.com>
2020-08-27 13:15:55 +0900

    2020/08/27 ホームページコーディング完了

--- music/js/slider.js
+++ music/js/slider.js
@@ -7,3 +7,10 @@ $(function(){
         $('.dept01').stop().slideUp(200);
     });
 });
+$(document).ready(function(){
+    $('.slider > ul').bxSlider({
+        auto: true,
+        mode: 'fade',
+        pager: false,
+    });
+});
